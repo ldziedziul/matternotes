18d348521692af4aca5920d6a1a26276264d9fbf
Lukasz Dziedziul <lukasz.dziedziul@cronn.de>
2016-12-04 20:49:28 +0100

    update help command

--- src/main/java/pl/dziedziul/matternotes/command/handler/HelpCommandHandler.java
+++ src/main/java/pl/dziedziul/matternotes/command/handler/HelpCommandHandler.java
@@ -17,17 +17,17 @@ public class HelpCommandHandler extends ActionBasedCommandHandler {
 	public SlashCommandResult handle(SlashCommand command) {
 		String message = "Command | Description\n" +
 			"------ | ------\n" +
-			"/notes my private note | Records 'my private note' as your note for the current channel/conversation. If one already exists, it will append it to your note\n" +
-			"/notes [title] my private note | Records 'my private note' under a 'title' notepad. If 'title' already exists, it will append it to your note\n" +
+			"/notes my private note | Creates 'my private note' as your note for the current channel/conversation. If one already exists, it will append it to your note\n" +
+			"/notes [title] my private note | Creates 'my private note' under a 'title' notepad. If 'title' already exists, it will append it to your note\n" +
 			"/notes show | Shows your notes for the current channel/conversation\n" +
-			"/notes show [title] | Shows your notes for the specified title. Square brackets are necessary and not part of the name\n" +
+			"/notes show [title] | Shows your notes with the specified title. Square brackets are necessary and not part of the name\n" +
 			"/notes show all | Show all your notes, grouped by channel/conversation\n" +
-			"/notes clear | Clears all your notes for the current channel. They can't be retrieved, so make sure you were done with them\n" +
-			"/notes clear [title] | Clears all your notes saved under 'title'. They can't be retrieved, so make sure you were done with them\n" +
-			"/notes undo | Removes your latest note for the current channel. Your latest appended message will be deleted\n" +
-			"/notes undo [title] | Removes your latest note for the 'title' notepad. Your latest appended message will be deleted\n" +
-			"/notes help | Displays this (hopefully) helpful list of commands\n" +
-			"/notes about | Shows some information about matternotes";
+			"/notes clear | Clears permanently all your notes for the current channel/conversation.\n" +
+			"/notes clear [title] | Clears permanently all your notes with the specified title.\n" +
+			"/notes undo | Undo your latest change for the note for the current channel, i.e. your latest appended message will be deleted\n" +
+			"/notes undo [title] | Undo your latest change for the note with the specified title, i.e. your latest appended message will be deleted\n" +
+			"/notes help | Displays this list of commands\n" +
+			"/notes about | Shows some information about Matternotes";
 		return new SlashCommandResult(message);
 	}
 }
